9588021df69f5395c97ec2ce2e6230b9fb396c04
Arpita <mailtoarpita972@gmail.com>
2022-11-28 22:42:38 +0530

    first version

---
+++ arptest/index.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function () {
+
+  
+
+// typing text animation script
+var typed = new Typed(".typing", {
+  strings: ["Welcome to", "OPTCL E-map digitization"],
+  typeSpeed: 100,
+  backSpeed: 30,
+  loop: true,
+});
+
+
+//For fixing header
+window.onscroll = function () {
+  myFunction();
+};
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+});
